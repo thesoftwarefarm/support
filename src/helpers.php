7c7ecfeb0475be64a9912eef257ff1596914239d
Ionut Antohi <ionut.antohi@gmail.com>
2026-03-05 09:53:35 +0200

    updates

--- src/helpers.php
+++ src/helpers.php
@@ -1,19 +1,13 @@
 <?php
 
-/*
- * Given a json encoded string and a default return value, it attempts to json_decode() the string.
- * If successful, the resulting value is returned; if an error occurs, the specified default return value
- * is returned instead.
- * */
-
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 if(!function_exists('from_json')) {
 
-    function from_json($json_encoded_string, $default_return = [], $assoc = false)
+    function from_json(mixed $json_encoded_string, mixed $default_return = [], bool $assoc = false): mixed
     {
-        $decoded = json_decode($json_encoded_string, $assoc);
+        $decoded = json_decode((string)$json_encoded_string, $assoc);
 
         if((json_last_error() !== JSON_ERROR_NONE))
         {
@@ -24,12 +18,9 @@ if(!function_exists('from_json')) {
     }
 }
 
-/*
- * Returns the MySQL date as a Carbon instance.
- * */
 if (!function_exists('carbon_mysql_now')) {
 
-    function carbon_mysql_now()
+    function carbon_mysql_now(): Carbon
     {
         $result = (array) DB::selectOne('select now() as now');
         return Carbon::parse($result['now']);
@@ -42,14 +33,12 @@ if(!function_exists('extract_date_range')) {
      * "Y-m-d"
      * "Y-m-d to Y-m-d"
      * "Y-m-d H:i to Y-m-d H:i"
-     *
-     * @param string $string
-     * @return object|null
      */
-    function extract_date_range($string = "")
+    function extract_date_range(?string $string = null): ?object
     {
-        if(empty($string))
+        if(!$string) {
             return null;
+        }
 
         if(preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $string, $matches))
         {
